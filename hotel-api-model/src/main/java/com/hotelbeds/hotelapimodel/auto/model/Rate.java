7d1c92150fbd03fa9bd8ba66ede7feb5eda54246
Stuart MacCallum <maccalsa@googlemail.com>
2018-06-21 16:12:17 +0100

    add daily rates json annotation, typo

--- hotel-api-model/src/main/java/com/hotelbeds/hotelapimodel/auto/model/Rate.java
+++ hotel-api-model/src/main/java/com/hotelbeds/hotelapimodel/auto/model/Rate.java
@@ -80,6 +80,7 @@ public class Rate extends BasicRate {
     private String boardName;
     @XmlElementWrapper(name = "cancellationPolicies")
     @XmlElement(name = "cancellationPolicy")
+    @JsonProperty("cancellationPolicies")
     private List<CancellationPolicy> cancellationPolicies;
     @XmlElement(name = "taxes")
     private Taxes taxes;
@@ -107,7 +108,7 @@ public class Rate extends BasicRate {
     @JsonSerialize(using = RateSerializer.class)
     private BigDecimal rateup;
     @XmlElementWrapper(name = "dailyRates")
-    @XmlElement(name = "dailyRate")
+    @XmlElement(name = "dailyRates")
     @JsonProperty("dailyRates")
     private List<DailyRate> dailyRates;
 
